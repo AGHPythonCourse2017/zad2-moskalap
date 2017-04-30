c36b36cb583f3b97729d4b1dfd639a3d0f6de045
moskalap <executeorder66>
2017-04-30 17:41:45 +0200

    Aglorytm prawie działa!

--- cmptcomplexity/scripts/controller.py
+++ cmptcomplexity/scripts/controller.py
@@ -11,24 +11,31 @@ class Controller:
         import os
         import signal
         import cmptcomplexity.scripts.exceptions as exceptions
-        def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+        def timeout():
             def decorator(func):
                 def _handle_timeout(signum, frame):
-                    raise exceptions.TimeoutCCExcetion(error_message)
+                    raise exceptions.TimeoutCCExcetion('ops')
 
                 def wrapper(*args, **kwargs):
+                    ltou, _ = signal.setitimer(signal.ITIMER_REAL, 0)
+                    signal.setitimer(signal.ITIMER_REAL, ltou)
                     signal.signal(signal.SIGALRM, _handle_timeout)
-                    signal.alarm(seconds)
+
                     try:
                         result = func(*args, **kwargs)
+                    except exceptions.TimeoutCCExcetion:
+                        return 0, 'except'
+
                     finally:
-                        signal.alarm(0)
-                    return result
+                        ltou, _ = signal.setitimer(signal.ITIMER_REAL, 0)
+                        signal.setitimer(signal.ITIMER_REAL, ltou)
+
+                    return result, ltou
 
                 return wraps(func)(wrapper)
 
             return decorator
-
+        @timeout()
         def avrg_measure(task, N, repeat, n):
             import timeit
             list = timeit.repeat(task.stringify_example_invoke(N), task.stringify_init(N), repeat=repeat, number=n)
@@ -45,16 +52,42 @@ class Controller:
         a_x2 = []
         a_log = []
         import math
-        for i in [10,100,1000,10000,100000,1000000]:
+        timeout = 50
+        signal.setitimer(signal.ITIMER_REAL, timeout)
+        import logging
+        can= True
+        import random
+        MIN =10
+        MAX =100
 
-            x.append(i) #firstmeasure
-            c_y = avrg_measure(self.task,i, 5,10)
-            y.append(c_y)
-            a_lin.append(c_y / i)
-            a_x2.append(c_y / (i ** 2))
-            a_log.append(c_y / (i * math.log2(i)))
+        timeo = 0.00001
+        while can:
+            if timeo == 0:
+                timeo=0.00001
+            i = random.randint(MIN, MAX)
+            logging.info('i= %s MAX= %s timeo =%f diff= %s', str(i), str(MAX), timeo,
+                         str((timeout - timeo) / float((timeo))))
+            timeout=timeo
 
+            c_y, timeo = avrg_measure(self.task,i, 5,10)
+            if timeo == 'except':
+                can = False
+            else:
+                if(timeo>0):
+                    if (timeout-timeo)/float((timeo)) < 0.1:
+                        MAX = i + MAX
+                    else:
+                        if MAX>100:
+                            MAX=MAX//10
 
+                    x.append(i)
+                    logging.info('to run alarm %s', str(timeo))
+                    y.append(c_y)
+                    a_lin.append(c_y / i)
+                    a_x2.append(c_y / (i ** 2))
+                    a_log.append(c_y / (i * math.log2(i)))
+
+        ltou, _ = signal.setitimer(signal.ITIMER_REAL, 0)
 
 
 
@@ -75,10 +108,13 @@ class Controller:
         print('x^2',quy)
         print('x',liny)
         print('xlogx',logy)
-        print ('y/x^2', [x / y for x, y in zip(y,quy)])
-        print ('y/x', [x / y for x, y in zip(y,liny)])
-        print ('y/xlogx', [x / y for x, y in zip(y,logy)])
-        plt.plot(x, y, x, liny, x, logy)
+        q = [(x - y)**2 for x, y in zip(y,quy)]
+        print ('y/x^2', sum(q)/len(q),q)
+        l = [(x - y)**2 for x, y in zip(y,liny)]
+        print ('y/x', sum(l)/len(l),l)
+        lg = [(x - y)**2 for x, y in zip(y,logy)]
+        print ('y/xlogx', sum(lg)/len(lg),lg)
+        plt.plot(x, y, x, liny, x, logy,x,quy)
         plt.show()
 
 
