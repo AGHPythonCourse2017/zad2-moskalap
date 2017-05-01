c4b676a321857ea9478eaf499f720b6c0155b352
moskalap <executeorder66>
2017-05-01 20:27:37 +0200

    Poprawiono solver, rozwiązywanie metodą General Least Squares

--- cmptcomplexity/scripts/solver.py
+++ cmptcomplexity/scripts/solver.py
@@ -1,75 +1,174 @@
 class Solver:
+    """Class responsible for solving a problem with received data from controller"""
+
     class Result:
-        def __init__(self, x, y, complexity, how_long=None, how_many=None):
+        """Container for a results"""
+
+        def __init__(self, x, y, complexity, sheet):
             self.x = x
             self.y = y
-            self.complexity = complexity
-            self.how_long = how_long
-            self.how_many = how_many
+            self.complexity_k = complexity
+            self.complexity = {
+                'linear_ratios': 'O(N)',
+                'square_ratios': 'O(N^2)',
+                'xlogx_ratios': 'O(N log N)',
+                'logx_ratios': 'O(log N)',
+                'constant': 'O(1)'
 
+            }[complexity]
+            self.sheet = sheet
 
-    def __init__(self,x,y):
+            fun_tuple = self.sheet[self.complexity_k]
+            import math
+            if len(fun_tuple) == 2:
+                p1, p2 = fun_tuple
+
+                fun_tuple = (p1, p2, [0])
+            print(fun_tuple)
+
+            self.fun = {
+
+                'linear_ratios': (
+                    lambda x: fun_tuple[0][0] + x * fun_tuple[1][0],
+                    str(fun_tuple[1][0]) + 'N+' + str(fun_tuple[0][0])),
+                'square_ratios': (lambda x: fun_tuple[0][0] + x * fun_tuple[1][0] + fun_tuple[2][0] * x ** 2,
+                                  str(fun_tuple[2][0]) + 'N^2+' + str(fun_tuple[1][0]) + 'N+' + str(fun_tuple[0][0])),
+                'xlogx_ratios': (lambda x: fun_tuple[0][0] * x * math.log2(x) + fun_tuple[1][0] * x,
+                                 str(fun_tuple[0][0]) + 'N log(' + str(
+                                     math.exp((fun_tuple[1][0] / fun_tuple[0][0]) ** 2)) + 'N)'),
+                'logx_ratios': (lambda x: fun_tuple[0][0] + math.log2(x) * fun_tuple[1][0],
+
+                                str(str(fun_tuple[1][0]) + 'log' + str(
+                                    math.exp(fun_tuple[1][0] / fun_tuple[0][0])) + str(fun_tuple[0][0]))),
+                'constant': (lambda x: sum(self.y) / len(self.y), 'O(1)')
+            }[complexity]
+
+        def show(self, title):
+
+            import matplotlib
+            matplotlib.use('Agg')
+            import matplotlib.pyplot as plt
+            import numpy as np
+            charts = self.sheet.keys()
+            MAX = max(self.y)
+            xrange = np.linspace(1, self.x[-1], len(self.x))
+
+            def prepare_chart(key):
+                if key != self.complexity_k:
+                    if key == 'logx_ratios':
+                        values = list(filter(lambda k: k <= MAX, np.log2(xrange) * self.sheet['logx_ratios']))
+                        plt.plot(xrange[0:len(values)], values, label='o(log N)', linestyle='--', color='b')
+                    if key == 'square_ratios':
+                        values = list(filter(lambda k: k <= MAX, np.power(xrange, 2) * self.sheet['square_ratios']))
+                        plt.plot(xrange[0:len(values)], values, label='o(N^2)', linestyle='--', color='r')
+                    if key == 'xlogx_ratios':
+                        values = list(filter(lambda k: k <= MAX, np.log2(xrange) * xrange * self.sheet['xlogx_ratios']))
+                        plt.plot(xrange[0:len(values)], values, label='o(N log N)', linestyle='--', color='g')
+                    if key == 'logx_ratios':
+                        values = list(filter(lambda k: k <= MAX, np.multiply(xrange, self.sheet['linear_ratios'])))
+                        plt.plot(xrange[0:len(values)], values, label='o(N)', linestyle='--', color='y')
+
+                else:
+                    if key == 'log_xratios':
+                        values = list(filter(lambda k: k <= MAX, [sum(self.y) / len(self.y) for i in xrange]))
+                        plt.plot(xrange[0:len(values)], values, label='o(1)', linestyle='--', color='y')
+
+                    values = list(filter(lambda k: k <= MAX, [self.fun[0](x) for x in xrange]))
+                    plt.plot(xrange[0:len(values)], values, label=self.fun[1], linestyle='--', color='k')
+
+            for x in charts:
+                prepare_chart(x)
+
+            plt.plot(self.x, self.y, label='measured times', marker='o', linestyle='', color='k')
+            plt.legend()
+            plt.title(str(title) + '\n' + self.complexity)
+
+            plt.xlabel('problem size[N]')
+            plt.ylabel('time [MSEC]')
+            plt.legend(loc=2, prop={'size': 6})
+            plt.show()
+            plt.savefig(title + '_plot.png', ext='png', bbox_inches='tight', dpi=1200)
+
+    def __init__(self, x, y):
 
         self.points = []
-        self.liny = []
-        self.quy = []
-        self.logy = []
-        self.a_lin = []
-        self.a_x2 = []
-        self.a_log = []
+        self.sheet = {
+            'linear_ratios': [],
+            'square_ratios': [],
+            'xlogx_ratios': [],
+            'logx_ratios': [],
+            'constant': []
+
+        }
         import math
 
         for i in range(len(x)):
-            self.points.append((x[i],y[i]))
+            self.points.append((x[i], y[i]))
 
         self.x, self.y = map(list, zip(*sorted(self.points, key=lambda k: k[0])))
 
         for i in range(len(x)):
-            self.a_lin.append(self.y[i] / self.x[i])
-            self.a_x2.append(self.y[i] / (self.x[i] ** 2))
-            self.a_log.append(self.y[i]/ (self.x[i] * math.log2(x[i])))
-
-        self.sheet={
-
-        }
-        self.a_ratio = sum(self.a_lin[0:10]) / len(self.a_lin[0:10])
-        self.ax2_ratio = sum(self.a_x2) / len(self.a_x2)
-
-        self.alog = self.a_log[10]
-        self.ref_x = []
-        for act_x in range(10,self.x[-1]):
-            self.ref_x.append(act_x)
-            self.quy.append(self.ax2_ratio * (act_x ** 2))
-            self.liny.append(self.a_ratio* act_x)
-            self.logy.append(self.alog*math.log2(act_x) * act_x)
-        self.solve()
-
+            self.sheet.get('linear_ratios').append(self.y[i] / self.x[i])
+            self.sheet.get('square_ratios').append(self.y[i] / (self.x[i] ** 2))
+            self.sheet.get('xlogx_ratios').append(self.y[i] / (self.x[i] * math.log2(self.x[i])))
+            self.sheet.get('logx_ratios').append(self.y[i] / (math.log2(self.x[i])))
+            self.sheet.get('constant').append(self.y[i])
 
     def solve(self):
-        import matplotlib.pyplot as plt
-        import numpy as np
+        # in sheet there are ratios
+        the_most_accurate = None
+        diff = 999999
 
+        def count_diff(x):
+            import math
+            avg = sum(x) / len(x)
+            diff = 0
+            for i in x:
+                diff += math.sqrt(((avg - i) / avg) ** 2)
+            return diff
 
-        print('x', self.x)
-        print('y', self.y)
-        print('x^2', self.quy)
-        print('x', self.liny)
-        print('xlogx', self.logy)
-        q = [(x - y) ** 2 for x, y in zip(self.y, self.quy)]
-        print('y/x^2', sum(q) / len(q), q)
-        l = [(x - y) ** 2 for x, y in zip(self.y, self.liny)]
-        print('y/x', sum(l) / len(l), l)
-        lg = [(x - y) ** 2 for x, y in zip(self.y, self.logy)]
-        print('y/xlogx', sum(lg) / len(lg), lg)
-        plt.plot(self.x, self.y, label='results', marker='o', color='r', linestyle='--')
-        plt.plot(self.ref_x, self.liny, label='teor_lin', linestyle='--')
-        plt.plot(self.ref_x, self.logy, label='teor_nlogn', linestyle='--')
-        self.max = self.y[-1]
+        def count_ratio(x):
+            return sum(x[1:3]) / len(x[1:3])
 
-        self.quy = list(filter(lambda k: k < self.max, self.quy))
-        plt.plot(self.ref_x[:len(self.quy)], self.quy[:len(self.quy)], label='teor n^2', linestyle='--')
-        plt.legend()
+        for key in self.sheet.keys():
+            new_diff = count_diff(self.sheet.get(key))
+            if new_diff < diff:
+                if the_most_accurate:
+                    self.sheet[the_most_accurate] = count_ratio(self.sheet.get(the_most_accurate))
 
-        plt.xlabel('problem size')
-        plt.ylabel('time [cpu ticks]')
-        plt.show()
+                the_most_accurate = key
+                diff = new_diff
+            else:
+                self.sheet[key] = count_ratio(self.sheet.get(key))
+
+        import math
+
+        def aproximate_leas_square(base):
+            import numpy as np
+            base_fun = base[0]
+
+            A = np.matrix([list([base_fun[i](x) for i in range(len(base_fun))]) for x in self.x])
+
+            AT = A.transpose()
+            A = np.dot(AT, A)
+            b = np.matrix([list([x]) for x in self.y])
+            Y = np.dot(AT, b)
+            from scipy.linalg import solve
+            X = solve(A, Y)
+
+            self.sheet[the_most_accurate] = tuple(X)
+
+        base = {
+            # (base, ratios)
+            'linear_ratios': ((lambda x: 1, lambda x: x), ('b', 'a')),
+            'square_ratios': ((lambda x: 1, lambda x: x, lambda x: x ** 2), ('c', 'b', 'a')),
+            'xlogx_ratios': ((lambda x: x * math.log2(x), lambda x: x), ('a', 'alogb')),
+            'logx_ratios': ((lambda x: 1, lambda x: math.log2(x)), ('alogb', 'a')),
+            'constant': (lambda x: sum(self.y) / len(self.y), 0, 0, ('const', 's'))
+        }
+        if the_most_accurate == 'constant':
+            self.sheet[the_most_accurate] = tuple([[1], [1]])
+        else:
+            aproximate_leas_square(base[the_most_accurate])
+
+        return self.Result(self.x, self.y, the_most_accurate, self.sheet)
