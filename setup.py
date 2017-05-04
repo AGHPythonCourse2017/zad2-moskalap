588e57289151af021ebc7628096879481c08fdd5
moskalap <executeorder66>
2017-05-04 12:50:15 +0200

    dodanie setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='cmptcomplexity',
+    version='',
+    packages=['cmptcomplexity', 'cmptcomplexity.test', 'cmptcomplexity.test.unit', 'cmptcomplexity.test.unusued',
+              'cmptcomplexity.scripts'],
+    url='',
+    license='',
+    author='przemek',
+    author_email='',
+    description=''
+)
