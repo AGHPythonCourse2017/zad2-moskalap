7d24da73c82927d2396ec93a1ee9fc925d62bcf8
moskalap <executeorder66>
2017-05-06 14:21:13 +0200

    PEP8 controller ok

--- cmptcomplexity/scripts/controller.py
+++ cmptcomplexity/scripts/controller.py
@@ -6,22 +6,26 @@ class Controller:
         self.timeout = timeout
 
     def get_data(self):
-        """Functions, which invokes timeit, collects data and sends to solver"""
+        """Function, which invokes timeit, collects data and sends to solver"""
 
         import signal
-
+        import cmptcomplexity.scripts.exceptions as excp
         from cmptcomplexity.scripts.decorators import timeout
 
         @timeout()
         def avrg_measure(task, N, repeat, n):
             import timeit
-            list = timeit.repeat(task.stringify_example_invoke(N), task.stringify_init(N), repeat=repeat, number=n)
+            msrs = timeit.repeat(task.stringify_example_invoke(N),
+                                 task.stringify_init(N), repeat=repeat,
+                                 number=n)
+            return (sum(msrs) / len(msrs)) / n
 
-            return (sum(list) / len(list)) / n
-
+        if self.timeout < 0.5:
+            raise excp.WrongTimeoutCCExcetion('Timeout must be > 0.5')
         timeout = self.timeout
         signal.setitimer(signal.ITIMER_REAL, timeout)
         import logging
+        logging.info('Started receiving data.')
         can = True
         import random
         MIN = 10
@@ -30,21 +34,23 @@ class Controller:
         y = []
         r = 5
         n = 10
-        timeo = 0.00001
+        to_alarm = self.timeout - 0.00001
         while can:
-            if timeo == 0:
-                timeo = 0.00001
+            if to_alarm == 0:
+                to_alarm = 0.00001  # prevents from /0
             i = random.randint(MIN, MAX)
-            logging.info('Checking time for N=%s range (0, %s)\nRemainging time=%fs', str(i), str(MAX), timeo)
-            timeout = timeo
+            logging.info('\t(%f %%)\tMeasuring time for N=%s range (0, %s).',
+                         (100 - to_alarm * 100.0 / self.timeout),
+                         str(i), str(MAX))
+            timeout = to_alarm
 
-            c_y, timeo = avrg_measure(self.task, i, r, n)
-            if timeo == 'except':
+            c_y, to_alarm = avrg_measure(self.task, i, r, n)
+            if to_alarm == 'except':
                 can = False
             else:
 
-                if timeo > 0.5:
-                    if (timeout - timeo) / float((timeo)) < 0.1:
+                if to_alarm > 0.5:
+                    if (timeout - to_alarm) / float((to_alarm)) < 0.1:
                         MAX = i + MAX
                         n += 1
                     else:
@@ -54,15 +60,16 @@ class Controller:
                             MAX //= 1.25
 
                     x.append(i)
-                    logging.info('to run alarm %s', str(timeo))
+
                     y.append(c_y * 1000)
                 else:
                     can = False
 
-        ltou, _ = signal.setitimer(signal.ITIMER_REAL, 0)
-
+        to_alarm, _ = signal.setitimer(signal.ITIMER_REAL, 0)
+        logging.info('\t(%f %%)\tMeasuring times finished.', 100.0)
+        logging.info('Started approximate received data.')
         import cmptcomplexity.scripts.solver as solver
         slvr = solver.Solver(x, y)
         results = slvr.solve()
-
+        logging.info('Finished!')
         return results
