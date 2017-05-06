2b18a3e69cb1eb33e1c98074f9b1c979bf202d87
moskalap <executeorder66>
2017-05-06 14:41:12 +0200

    dodano wronttimeout exception

--- cmptcomplexity/scripts/exceptions.py
+++ cmptcomplexity/scripts/exceptions.py
@@ -9,6 +9,17 @@ class TimeoutCCExcetion(ComputeComplexityException):
     def __str__(self):
         return self.val
 
+
+class WrongTimeoutCCExcetion(ComputeComplexityException):
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        return self.val
+
+
+
+
 class InitializationError:
     def __init__(self, val):
         self.val=val
