9a3c3419706917b157cb86960cfca98584fabfad
moskalap <executeorder66>
2017-05-06 23:48:39 +0200

    usunięto nieużywany plik

--- cmptcomplexity/scripts/strings.py
+++
@@ -1,8 +0,0 @@
-class Strings():
-    import datetime
-    now =datetime.datetime.now()
-    s = now.strftime('%Y-%m-%d')
-    LOGS = './cmptcomplexity/logs/'+s+'.log'
-    INIT_CODE = './temporary_files/init.py'
-    EXAMINED_CODE = './temporary_files/examined_code.py'
-    CLEAN_UP_CODE = './temporary_files/clean_up_code.py'
